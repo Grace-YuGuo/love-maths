f6bfaaf27707640a4d5ee3c2e41ec3e4d60142d8
Yu Guo <grace.guo012@gmail.com>
2024-06-17 12:27:58 +0000

    Fea: Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,12 @@ document.addEventListener("DOMContentLoaded", function () {
                 alert("You clicked the submit");
             }else {
                 let gameType=this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+    runGame("addition");
+
 });
 
 
@@ -24,12 +26,20 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main game "loop", called when the script is first loaded
 *and after the user's answer has been processed
 */
-function runGame() {
+function runGame(gameType) {
+    //Creates two randome numbers between 1 and 25
       let num1=Math.ceil(Math.random()*25);
       let num2=Math.ceil(Math.random()*25);
+
+      if(gameType==="addition"){
+        displayAdditionQuestion(num1,num2);
+      }else{
+        alert(`Unknow game type: ${gameType} `);
+        throw `Unknow game type: ${gameType}. Aborting!`;
+      }
 }
 
-runGame();
+
 function checkAnswer() {
 
 }
@@ -46,8 +56,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1,operand2) {
+    document.getElementById("operand1").textContent=operand1;
+    document.getElementById("operand2").textContent=operand2;
+    document.getElementById("operator").textContent="+";
 }
 
 function displaySubtractQuestion() {
